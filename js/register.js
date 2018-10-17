5534cd7972f45fdf80e1105b22c1102d4505c739
Marcelo Javelly <maja18aj@student.cbs.dk>
2018-10-17 12:53:38 +0200

    Sign in form finally saving multiple users

--- js/register.js
+++ js/register.js
@@ -1,15 +1,21 @@
-   // Name and Password from the register-form
-  var inputGivenName = document.getElementById('givenName');
-  var inputFamilyName = document.getElementById('familyName');
-  var inputEmail = document.getElementById('email');
-  var inputPassword = document.getElementById('password');
+var users = [];
+// storing input from register-form
+function store() {
+    var inputGivenName = document.getElementById('givenName').value;
+    var inputFamilyName = document.getElementById('familyName').value;
+    var inputEmail = document.getElementById('email').value;
+    var inputPassword = document.getElementById('password').value;
+    // Create a new user object var user1 = new User(firstName, lastName.....)
+    var user = {inputGivenName, inputFamilyName, inputEmail, inputPassword};
+     
+ // JSON has two functions: stringify(Type:Any) AND parse(Type:String)
+    // var user = JSON.stringify(newuser); //create new type of user (newuser)
+    //NOT inside this function: 
+    //Use JSON.parse(*insert your string from locatStorage) and then assign the return value to a user object
+    users.push(user);
+    
+    var usersString = JSON.stringify(users);
+    
+    localStorage.setItem('users', usersString);
 
-  // storing input from register-form
-  function store() {
-      localStorage.setItem('givenName', inputGivenName.value);
-      localStorage.setItem('familyName', inputFamilyName.value);
-      localStorage.setItem('email', inputEmail.value);
-      localStorage.setItem('password', inputPassword.value);
-  }
-
-//Should add Repeat password, and password check...
+}
