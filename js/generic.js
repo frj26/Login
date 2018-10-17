1ecb59f9beff552353cdc7cd613d5a344219bc13
Frederik Rysgaard Jeppesen <frysgaard@gmail.com>
2018-10-17 11:33:39 +0200

    added generic stuff, js and css

--- js/generic.js
+++ js/generic.js
@@ -1,8 +1,13 @@
 // Footer that is used for various pages - easier to call this than make a new one.
-// If the footer is undefined - this footer will be called (in case we use it)
-
+// If the footer is undefined - this footer will be called (in case we use it)??
+// The IF-statement is done in order to failure handle. If we don't do it there will be a TypeError:
 var footer =  document.getElementById("footer");
 if(footer !== null){
-  footer.innerHTML = "<p>En rigtig fint tekst</p>";
+  footer.innerHTML = "<p class='footer-p'>This is our footer</p>";
 }
 
+var navbar = document.getElementById("navbar");
+if(navbar !== null){
+  navbar.innerHTML = "<p class='navbar-p'></p>";
+    
+}
