5143b811ef24b7d43a8e9a8d7bca4b245b964590
Marcelo Javelly <maja18aj@student.cbs.dk>
2018-10-18 13:12:58 +0200

    Sign in form now has the password restrictions

--- js/register3.js
+++ js/register3.js
@@ -1,18 +1,32 @@
+    // storing input from register-form
 var users = [];
-// storing input from register-form
-function store() {
+
+var submit = document.getElementById("submit");
+    //This starts the function when click in submit.
+submit.addEventListener("click",
+    //Calling the values from the html.
+function store(){
     var inputGivenName = document.getElementById('givenName').value;
     var inputFamilyName = document.getElementById('familyName').value;
     var inputEmail = document.getElementById('email').value;
     var inputPassword = document.getElementById('password').value;
-    var inputRepeatPassword = document.getElementById('repeatPassword');
-
+    var inputRepeatPassword = document.getElementById('repeatPassword').value;
+    var passwordLength = inputPassword.length;
+    //For the password length check.
+    if(passwordLength<8){
+        alert("Password must contain at least 8 characters.")
+        }
+    else{
+    //Password check.
+    if(inputPassword !== inputRepeatPassword){
+        alert("Passwords doesn't match.")
+        }
+    else{
+    //All fields filled out check.
     if(inputGivenName && inputFamilyName && inputEmail && inputPassword && inputRepeatPassword){
-//Eh aqui el problema. Ver que funcione bien el if.
-    // Create a new user object var user1 = new User(firstName, lastName.....)
+
     var user = {inputGivenName, inputFamilyName, inputEmail, inputPassword, repeatPassword};
-     
- // JSON has two functions: stringify(Type:Any) AND parse(Type:String)
+    // JSON has two functions: stringify(Type:Any) AND parse(Type:String)
     // var user = JSON.stringify(newuser); //create new type of user (newuser)
     //NOT inside this function: 
     //Use JSON.parse(*insert your string from locatStorage) and then assign the return value to a user object
@@ -25,8 +39,9 @@ function store() {
     document.location.href = "login.html";
     }
     else{
-        alert("Fill out all the fields to register, and/or make sure the passwords match.");
+        alert("Please fill out all the fields to register.");
+        }
     }
   }
-
-    // window.location.href= "login.html" //Ver como le puedo hacer para que meta varios.
+}
+  );
