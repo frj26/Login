fba2e6c46329339f9b7e275696ec4d35fc9125b8
Frederik Rysgaard Jeppesen <frysgaard@gmail.com>
2018-11-11 09:58:52 +0100

    ...

--- js/generic.js
+++ js/generic.js
@@ -12,7 +12,4 @@ if(navbar !== null) {
 // Footer that is used for various pages - easier to call this than make a new one.
 // If the footer is undefined - this footer will be called (in case we use it)?
 // The IF-statement is done in order to failure handle. If we don't do it there will be a TypeError:
-var footer =  document.getElementById("footer");
-if(footer !== null){
-  footer.innerHTML = "<p class='footer-p'>This is our footer</p>";
-}
+
