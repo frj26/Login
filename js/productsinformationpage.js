a4ca552fab62b6141f3ba9de68dd6ccf2f897ff8
Mie Høiby <miehoiby@MacBook.local>
2018-11-16 11:27:31 +0100

    comments to several files

--- js/productsinformationpage.js
+++ js/productsinformationpage.js
@@ -13,16 +13,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides"); //ByClasNname is the same as ById, the div in html is just given a class name instead of an ID
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {slideIndex = 1} // This if statement resets the slideIndex back to one, once the last slide is reached.
+  if (n < 1) {slideIndex = slides.length} // This if statement make sure the last image is shown as the next one if the user clicks on the left arrow when the first slide is showing.
+  for (i = 0; i < slides.length; i++) { //The images start at picture 1. When clicking on the next button, the picture count is increased with 1 which results in showing next image.
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block"; //This is needed so only one image is shown at a time.
   dots[slideIndex-1].className += " active";
 } 
